e9f4b196d02ed51d04598d061a4f9f223dc8b5ee
X00050327 <jamescunningham773@gmail.com>
2016-11-04 16:03:34 +0000

    First commit

---
+++ HelloWorld/src/app/test-new/test-new.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-new',
+  templateUrl: './test-new.component.html',
+  styleUrls: ['./test-new.component.css']
+})
+export class TestNewComponent implements OnInit {
+
+  msg: string; // new property message
+
+  constructor() {
+    this.msg = "New Component";
+   }
+
+  ngOnInit() {
+  }
+
+}
